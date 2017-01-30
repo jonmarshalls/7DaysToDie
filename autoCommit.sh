31eabcda316a04c8695e8f03ac16c2cfc18ab303
jonmarshalls <jonathan@j-marshall.co.uk>
2017-01-30 10:54:35 +0000

    Auto Commit 2017/01/30/ 10:54

--- autoCommit.sh
+++ autoCommit.sh
@@ -3,8 +3,15 @@ timestamp() {
 }
 
 
+cd /home/steam/.local/share 
 
+
+if git diff-index --cached --quiet HEAD --ignore-submodules --
+then
 echo "Auto Commit $(timestamp)"
 git add -A
 git commit -m "Auto Commit $(timestamp)"
 git push origin master
+else
+    echo "No changes"
+fi
